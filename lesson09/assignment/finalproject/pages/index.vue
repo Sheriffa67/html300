89d5650605e63f1b828e49607dc7ec0faa85b3a2
bdiallo10 <53807407+bdiallo10@users.noreply.github.com>
2020-06-05 17:17:13 -0700

    pushing current progress to git hub

--- lesson09/assignment/finalproject/pages/index.vue
+++ lesson09/assignment/finalproject/pages/index.vue
@@ -1,13 +1,49 @@
 <template>
   <div class="container">
-    <h1>This is my Final Project</h1>
+    <div class="navbar">
+        <TheNavigation />
+    </div>
+    <h1>The one place for all your traveling need</h1>
+    <p>From Brazil to africa, you can find information on how to travel</p>
+
+    <footer>
+      <TheNavigation />
+    </footer>
   </div>
+
 </template>
 
 <script>
 
+import TheNavigation from "@/components/TheNavigation";
+import TheBlog from "@/components/TheBlog";
+
+
+export default {
+  components: {
+    TheNavigation
+  },
+  // components: {
+  //   TheBlog
+  // }
+
+//  data: function () {
+//    return {
+//      pics: [
+//        {url: require('assets/brazil.jpg'), alt: 'Picture of Brazil'},
+//        {url: require('assets/hawaii.jpg'), alt: 'picture of hawaii'},
+//        {url: require('assets/jamaica.jpg'), alt: 'picture of jamaica'},
+//        {url: require('assets/panama.jpg'), alt: 'picture of panama'},
+//      ]
+//    }
+//  }
+};
+
 </script>
 
-<style>
-
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
 </style>
